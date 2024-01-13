44983acda1b9c6ee67dc3ab7e0af90d78fe4786e
Jens <49454015+Xentraxx@users.noreply.github.com>
2024-01-13 01:08:28 +0100

    Update downloadPrusaSlicer.sh
    Changed the prusa download to the current version and changed the path to a relative path, so the script also works if the executing user has a different name than pi.

--- octoprint_slicer/static/scripts/downloadPrusaSlicer.sh
+++ octoprint_slicer/static/scripts/downloadPrusaSlicer.sh
@@ -1,19 +1,19 @@
 # This script is to download PrusaSlicer on OctoPi OS
 
 # Move to the home directory
-cd /home/pi
+cd $HOME
 
 # Create a folder for PrusaSlicer
-mkdir /home/pi/PrusaSlicer-2.4.2
+mkdir $HOME/PrusaSlicer-2.7.1
 
 # Download PrusaSlicer
-wget https://github.com/prusa3d/PrusaSlicer/releases/download/version_2.4.2/PrusaSlicer-2.4.2+linux-armv7l-GTK2-202204251109.tar.bz2
+wget https://github.com/prusa3d/PrusaSlicer/releases/download/version_2.7.1/PrusaSlicer-2.7.1+linux-armv7l-GTK2-202312121430.tar.bz2
 
 # Extract PrusaSlicer
-tar -xvf PrusaSlicer-2.4.2+linux-armv7l-GTK2-202204251109.tar.bz2 -C /home/pi/PrusaSlicer-2.4.2 --strip-components=1
+tar -xvf PrusaSlicer-2.7.1+linux-armv7l-GTK2-202312121430.tar.bz2 -C $HOME/PrusaSlicer-2.7.1 --strip-components=1
 
 # Remove the downloaded file
-rm PrusaSlicer-2.4.2+linux-armv7l-GTK2-202204251109.tar.bz2
+rm PrusaSlicer-2.7.1+linux-armv7l-GTK2-202312121430.tar.bz2
 
 # Change permissions to the PrusaSlicer program
-chmod a+x /home/pi/PrusaSlicer-2.4.2/prusa-slicer
+chmod a+x $HOME/PrusaSlicer-2.7.1/prusa-slicer
